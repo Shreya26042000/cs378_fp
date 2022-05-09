110859fc20fb1d9a9de92ee4ab3316ec40436c8c
Shreya26042000 <shreya2000@live.in>
2022-05-09 00:58:10 +0000

    some more checklist code

--- run_checklist.py
+++ run_checklist.py
@@ -1,8 +1,8 @@
 import checklist
-# from checklist import Editor
-# from checklist import Perturb
-
+import checklist.editor as Editor
+import checklist.perturb as Perturb
 import datasets
+from nltk.corpus import wordnet as wn
 
 default_datasets = {'qa': ('squad',), 'nli': ('snli',)}
         # dataset_id = tuple(args.dataset.split(':')) if args.dataset is not None else \
@@ -16,7 +16,33 @@ dataset = datasets.load_dataset(*dataset_id)
 
 test_dataset = dataset['test']
 # TODO: Can add training examples here
-max_samples = 1000
+max_samples = 10
 test_dataset = test_dataset.select(range(max_samples))
 
+editor = Editor.Editor()
+print (editor.lexicons.keys())
 print(test_dataset)
+
+# for i in test_dataset:
+#   print(i)
+
+ADJ, ADJ_SAT, ADV, NOUN, VERB = "a", "s", "r", "n", "v"
+
+for i in wn.all_synsets():
+    if i.pos() in ['a', 's']: # If synset is adj or satelite-adj.
+        for j in i.lemmas(): # Iterating through lemmas for each synset.
+            if j.antonyms(): # If adj has antonym.
+                # Prints the adj-antonym pair.
+                print(j.name(), j.antonyms()[0].name())
+
+
+
+
+
+
+
+
+
+
+
+
